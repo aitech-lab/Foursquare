697bbc597d04674eb85381f1eb25b266c2710ab3
peko <peko@gasubasu.com>
2012-06-20 15:33:09 +0400

    some small changes

--- stat/foursquare-get-recent-friends-checkins.py
+++ stat/foursquare-get-recent-friends-checkins.py
@@ -1,4 +1,5 @@
 import httplib
+import os
 import sys
 sys.path.append('lib/python')
 
@@ -7,11 +8,12 @@ import time
 import tokens
 import random
 
-file = open("data/friends-checkins.txt", "a")
+file = open( os.path.dirname(os.path.realpath(__file__)) + "/data/friends-checkins.txt", "a")
 
 timestamp = int(time.time()) - 60*60*24
 
 while 1 :
+    print "[GET RECENT FRIENTDS CHECKINS]"
     conn = httplib.HTTPSConnection("api.foursquare.com");
     conn.request("GET",
         "/v2/checkins/recent"
@@ -23,7 +25,7 @@ while 1 :
     
     resp = conn.getresponse()
     print resp.status, resp.reason
-    timestamp = int(time.time())
+    timestamp = int(time.time())-15
 
     if (resp.status == 200) :
         d = json.loads(resp.read())
@@ -36,7 +38,7 @@ while 1 :
 
             print uid, " "*(8-len(uid)), vid, lat, " "*(13-len(lat)), lng, " "*(13-len(lng)), utm  
             file.write("%s, %s, %s, %s, %s\n" % (uid, vid, lat, lng, utm))
-
+    file.flush()
     # sleep 10 min
     print "sleeping 60sec"
     time.sleep(60)
